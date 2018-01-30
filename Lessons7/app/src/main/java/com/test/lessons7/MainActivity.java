743dfd133df512148147dea5026b9d73d215b61b
Andrey <arhsota@gmail.com>
2018-01-30 08:37:14 +0500

    learning Lesson 8

--- Lessons7/app/src/main/java/com/test/lessons7/MainActivity.java
+++ Lessons7/app/src/main/java/com/test/lessons7/MainActivity.java
@@ -2,6 +2,8 @@ package com.test.lessons7;
 //* Lesson 8
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +22,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.hello_tv);
-        Button button = findViewById(R.id.hello_btn);
+        final Button button = findViewById(R.id.hello_btn);
         editText = findViewById(R.id.name_et);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().trim().length() > 0) {
+                    button.setEnabled(true);
+                } else {
+                    button.setEnabled(false);
+                }
+
+            }
+        });
 
         button.setOnClickListener(onClickListener);
     }
@@ -29,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            textView.setText("Hi!");
+            String name = editText.getText().toString();
+            textView.setText("Hi, " + name);
         }
     };
 
