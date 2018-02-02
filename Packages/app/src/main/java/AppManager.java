8d0dbb38a63c2acd2600f26de0955dbea5e73a9e
Andrey <arhsota@gmail.com>
2018-02-02 15:31:32 +0500

    package

---
+++ Packages/app/src/main/java/AppManager.java
@@ -0,0 +1,38 @@
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 02.02.2018.
+ */
+
+public class AppManager {
+    private final PackageManager packageManager;
+
+    public AppManager(Context context) {
+        packageManager = context.getPackageManager();
+
+    }
+    public List<AppInfo> getInstalledApps() {
+        List<AppInfo> installedApps = new ArrayList<>();
+
+        List<PackageInfo> installedPackages = packageManager.getInstalledPackages(0);
+
+        for (PackageInfo installedPackage : installedPackages) {
+            AppInfo appInfo = new AppInfo(
+                    installedPackage.packageName, // Имя пакета
+                    installedPackage.versionCode, // Код версии
+                    installedPackage.versionName, // Имя версии
+                    installedPackage.applicationInfo.loadLabel(packageManager).toString(), // Имя приложения
+                    installedPackage.applicationInfo.loadIcon(packageManager) // Иконка приложения
+            );
+
+            installedApps.add(appInfo);
+        }
+
+        return installedApps;
+    }
+}
