8bc3cb6ddee2e97e5e0feb7e981c9d3b14a438db
Andrey <arhsota@gmail.com>
2018-02-02 17:49:12 +0500

    package

--- Packages/app/src/main/java/com/test/packages/MainActivity.java
+++ Packages/app/src/main/java/com/test/packages/MainActivity.java
@@ -2,12 +2,22 @@ package com.test.packages;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private AppManager appManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Debug!");
+        Log.w("MainActivity", "Warning!");
+        Log.v("MainActivity", "Verbose!");
+        Log.i("MainActivity", "Info!");
+        Log.e("MainActivity", "ERROR!");
+        Log.wtf("MainActivity", "WTF???!");
     }
 }
