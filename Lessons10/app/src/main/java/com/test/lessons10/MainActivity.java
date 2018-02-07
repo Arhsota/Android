6070d952136cffecd69dd0802f3f2729a9c9f6a0
Andrey <arhsota@gmail.com>
2018-02-07 10:21:48 +0500

    lesson10

---
+++ Lessons10/app/src/main/java/com/test/lessons10/MainActivity.java
@@ -0,0 +1,17 @@
+package com.test.lessons10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.image_iv);
+        imageView.setImageResource(R.drawable.image);
+    }
+}
