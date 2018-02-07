d76ae98ba6562519d6cd43a9dc794fc4dcae4713
Andrey <arhsota@gmail.com>
2018-02-07 09:31:44 +0500

    lesson13

--- Packages/app/src/main/java/com/test/packages/MainActivity.java
+++ Packages/app/src/main/java/com/test/packages/MainActivity.java
@@ -1,7 +1,9 @@
 package com.test.packages;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private AppManager appManager;
+    private AppsAdapter appsAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +24,43 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(onRefreshListener);
+
         appManager = new AppManager(this);
-        List<AppInfo> installedApps = appManager.getInstalledApps();
 
         AppsAdapter appsAdapter = new AppsAdapter();
 
+
         RecyclerView recyclerView = findViewById(R.id.apps_rv);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
         recyclerView.setAdapter(appsAdapter);
 
+
+
+        reloadApps();
+    }
+
+    private void reloadApps() {
+        List<AppInfo> installedApps = appManager.getInstalledApps();
         appsAdapter.setApps(installedApps);
         appsAdapter.notifyDataSetChanged();
+        swipeRefreshLayout.setRefreshing(false);
+
     }
+
+
+
+
+    private final SwipeRefreshLayout.OnRefreshListener onRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            reloadApps();
+        }
+    };
 }
