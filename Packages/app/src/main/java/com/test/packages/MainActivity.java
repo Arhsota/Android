df48189bdf096868c68837cc13f551484f0b9fdc
Andrey <arhsota@gmail.com>
2018-02-07 09:26:45 +0500

    package

--- Packages/app/src/main/java/com/test/packages/MainActivity.java
+++ Packages/app/src/main/java/com/test/packages/MainActivity.java
@@ -2,6 +2,7 @@ package com.test.packages;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
@@ -15,12 +16,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.apps_rv);
-
         appManager = new AppManager(this);
+        List<AppInfo> installedApps = appManager.getInstalledApps();
 
+        AppsAdapter appsAdapter = new AppsAdapter();
 
+        RecyclerView recyclerView = findViewById(R.id.apps_rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(appsAdapter);
+
+        appsAdapter.setApps(installedApps);
+        appsAdapter.notifyDataSetChanged();
     }
 }
