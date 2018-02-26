a4d45b14a342baed77c2424e83e7c12dc23a0d04
Arhsota <arhsota@gmail.com>
2018-02-26 22:43:43 +0300

    only checking with 11 to understand working or not

---
+++ IMT/app/src/main/java/com/arhsota/android/imt/MainActivity.java
@@ -0,0 +1,68 @@
+package com.arhsota.android.imt;
+
+//* my first real soft based on lesson 8 Skillberg
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.hello_tv);
+        final Button button = findViewById(R.id.hello_btn);
+        editText = findViewById(R.id.name_et);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().trim().length() > 0) {
+                    button.setEnabled(true);
+                } else {
+                    button.setEnabled(false);
+                }
+
+            }
+        });
+
+        button.setOnClickListener(onClickListener);
+    }
+
+
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String name = editText.getText().toString();
+            if (name.equals ("11")) {
+                textView.setText(getString(R.string.name_text_format, name));
+            }
+            else {
+                textView.setText(getString(R.string.name_text_format_normal, name));
+            }
+
+        }
+
+    };
+}
