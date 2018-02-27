fdffde693779018bc83d2a4fa468e84cf23b6a8c
Arhsota <arhsota@gmail.com>
2018-02-27 21:21:31 +0300

    full working programm, but with manual entering data, not spinner

--- IMT/app/src/main/java/com/arhsota/android/imt/MainActivity.java
+++ IMT/app/src/main/java/com/arhsota/android/imt/MainActivity.java
@@ -14,18 +14,25 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
-    private EditText editText;
+    private EditText editTextW;
+    private EditText editTextL;
     private Button button;
+    private   double myweight;
+    private   double mylength;
+    private   double myresult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.hello_tv);
-        final Button button = findViewById(R.id.hello_btn);
-        editText = findViewById(R.id.name_et);
-        editText.addTextChangedListener(new TextWatcher() {
+        textView = findViewById(R.id.result_out);
+        final Button button = findViewById(R.id.calculate_btn);
+        editTextW = findViewById(R.id.weight);
+        editTextL = findViewById(R.id.length);
+
+//        reading weight
+        editTextW.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -43,9 +50,31 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     button.setEnabled(false);
                 }
-
             }
-        });
+          }
+        );
+
+        button.setOnClickListener(onClickListener);
+// reading length
+        editTextL.addTextChangedListener(new TextWatcher() {
+            @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                 }
+
+            @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                 }
+
+            @Override
+                 public void afterTextChanged(Editable editable) {
+                    if (editable.toString().trim().length() > 0) {
+                      button.setEnabled(true);
+                   } else {
+                      button.setEnabled(false);
+                   }
+             }
+        }
+        );
 
         button.setOnClickListener(onClickListener);
     }
@@ -54,12 +83,25 @@ public class MainActivity extends AppCompatActivity {
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            String name = editText.getText().toString();
-            if (name.equals ("11")) {
-                textView.setText(getString(R.string.name_text_format, name));
+
+            String weight = editTextW.getText().toString();
+            String length = editTextL.getText().toString();
+
+
+            myweight = Double.parseDouble(weight);
+            mylength = Double.parseDouble(length);
+            myresult = myweight/(mylength * mylength); //calculatin IMT
+
+
+//            String myresultStr = Double.toString(myresult);
+            String myresultStrFormat = String.format("%.2f",myresult);
+
+//            if (weight.equals ("11")) {                // for string
+            if (myresult >= 30) {
+                textView.setText(getString(R.string.name_text_format,myresultStrFormat));
             }
             else {
-                textView.setText(getString(R.string.name_text_format_normal, name));
+                textView.setText(getString(R.string.name_text_format_normal,myresultStrFormat));
             }
 
         }
