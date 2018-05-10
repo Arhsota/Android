72ba38440c7b092cbd48a30e2f8a6cd16d5b7f6f
Administrator <arhsota@gmail.com>
2018-05-10 16:13:23 +0500

    2-st commtit, to git for help to work at hotel and vice a verse

--- notes/app/src/main/java/com/notes/android/arhsota/notes/MainActivity.java
+++ notes/app/src/main/java/com/notes/android/arhsota/notes/MainActivity.java
@@ -3,7 +3,39 @@ package com.notes.android.arhsota.notes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static android.provider.BaseColumns._ID;
+import static com.notes.android.arhsota.notes.db.NotesContract.Notes.COLUMN_CREATED_TS;
+import static com.notes.android.arhsota.notes.db.NotesContract.Notes.COLUMN_NOTE;
+import static com.notes.android.arhsota.notes.db.NotesContract.Notes.COLUMN_TITLE;
+import static com.notes.android.arhsota.notes.db.NotesContract.Notes.COLUMN_UPDATED_TS;
+import static com.notes.android.arhsota.notes.db.NotesContract.Notes.TABLE_NAME;
+/*
+import static com.notes.android.arhsota.notes.db.NotesContract.Notes.COLUMN_CREATED_TS;
+import static com.notes.android.arhsota.notes.db.NotesContract.Notes.COLUMN_NOTE;
+import static com.notes.android.arhsota.notes.db.NotesContract.Notes.COLUMN_TITLE;
+import static com.notes.android.arhsota.notes.db.NotesContract.Notes.COLUMN_UPDATED_TS;
+import static com.notes.android.arhsota.notes.db.NotesContract.Notes.TABLE_NAME;
+*/
+
 public class MainActivity extends AppCompatActivity {
+    public static final String CREATE_TABLE =
+            String.format("CREATE TABLE %s " +
+                            "(%s INTEGER PRIMARY KEY, " +
+                            "%s TEXT NOT NULL, " +
+                            "%s TEXT NOT NULL, " +
+                            "%s INTEGER NOT NULL, " +
+                            "%s INTEGER NOT NULL);",
+                    TABLE_NAME,
+                    _ID,
+                    COLUMN_TITLE,
+                    COLUMN_NOTE,
+                    COLUMN_CREATED_TS,
+                    COLUMN_UPDATED_TS);
+    public static final String CREATE_UPDATED_TS_INDEX =
+            String.format("CREATE INDEX updated_ts_index " +
+                            "ON %s (%s);",
+                    TABLE_NAME,
+                    COLUMN_UPDATED_TS);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- notes/app/src/main/java/com/notes/android/arhsota/notes/db/NotesContract.java
+++ notes/app/src/main/java/com/notes/android/arhsota/notes/db/NotesContract.java
@@ -3,14 +3,23 @@ package com.notes.android.arhsota.notes.db;
 import android.provider.BaseColumns;
 
 public final class NotesContract {
-    private NotesContract() {
-}
+
+        public static final String DB_NAME = "notes.db";
+        public static final int DB_VERSION = 1;
+        public static final String[] CREATE_DATABASE_QUERIES
+                = {
+                   Notes.CREATE_TABLE,
+                   Notes.CREATE_UPDATED_TS_INDEX
+        };
+
     public static abstract class Notes implements
             BaseColumns {
+        public static final String CREATE_TABLE = "create_table";
         public static final String TABLE_NAME = "notes";
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_NOTE = "note";
         public static final String COLUMN_CREATED_TS = "created_ts";
         public static final String COLUMN_UPDATED_TS = "updated_ts";
+        public static final String CREATE_UPDATED_TS_INDEX = "create_updated_ts_index";
     }
 }
