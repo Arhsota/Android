5062f58f509f4aaadcf1f968e0273fb9584f23c2
Arhsota <arhsota@gmail.com>
2019-01-13 23:26:05 +0300

    creating dimenses and land layout force

--- IMT/app/src/main/java/com/arhsota/android/imt/MainActivity.java
+++ IMT/app/src/main/java/com/arhsota/android/imt/MainActivity.java
@@ -1,42 +1,58 @@
 package com.arhsota.android.imt;
 
-// version 1.6
-// changes in this version - make different size of text due to resolution
+// version 2.1
+// changes in this version - second activity with correspondent features and banner on every activity
+// using constraint layout
 // my first real soft based on lesson 8 Skillberg
 // calculating index body fat based on your weight and length both for male and female
-// Sevastyanov Andrey, 2018, may
-// Sabetta
-// Special thanks to A. Bakulina for testing this app
+// Sevastyanov Andrey, 2018, september
+// Arkhangelsk
+//
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+
+
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
+
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.firebase.analytics.FirebaseAnalytics;
+//import com.google.firebase.analytics.FirebaseAnalytics;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
 //import com.google.android.gms.ads.reward.RewardedVideoAd;
 
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private AdView mAdView;
-    private FirebaseAnalytics mFirebaseAnalytics;
+//    private FirebaseAnalytics mFirebaseAnalytics;
+    private ShareActionProvider shareActionProvider;
 
  //   private RewardedVideoAd mRewardedVideoAd;
 
     private TextView textView;
-    private TextView textViewW;
+ /*   private TextView textViewW;
     private TextView textViewL;
     private TextView textViewAge;
     private TextView textViewTable3;
@@ -45,11 +61,22 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewTable6;
     private TextView textViewTable7;
     private TextView textViewTable8;
+    */
+    private TextView textViewTable;
     private EditText editTextW;
     private EditText editTextL;
     private EditText editTextAge;
 //    private Button button;
     private   double myweight;
+    // str_XXX for intent
+    private   String str_IMT ;
+    private   String str_IMT_mail ;
+
+    private   String str_Weight= "0";
+    private   String str_Length = "0";
+    private   String str_Age = "0";
+    private   String str_Date = "0";
+
     private   double mylength;
     private   double myage;
     private   double myresult;
@@ -58,19 +85,116 @@ public class MainActivity extends AppCompatActivity {
     private   boolean fillTextL = false;
     private   boolean fillTextA = false;
 
+    final String LOG_TAG = "myLogs";
+
+    final String FILENAME = "file";
+
+    final String DIR_SD = "MyFiles";
+    final String FILENAME_SD = "fileSD";
+
+
     private final String YOUR_ADMOB_APP_ID = "ca-app-pub-7279174300665421~3105181624";
+
+
+    @Override    public boolean onCreateOptionsMenu(Menu menu) {
+        // Заполнение меню; элементы (если они есть) добавляются на панель действий.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+      //  Intent intent = new Intent(Intent.ACTION_SENDTO);
+    //   Intent intent = new Intent(Intent.ACTION_SEND);
+    //    intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+    //    intent.setType("text/plain");
+     //   Toast.makeText(MainActivity.this, "111111111111111",
+     //           Toast.LENGTH_LONG).show();
+    //    intent.putExtra(Intent.EXTRA_TEXT, str_Date + str_IMT+"22222");
+    //    intent.putExtra("IMT", str_IMT);
+   //     shareActionProvider.setShareIntent(intent);
+     //   setIntent("Дата: " + date + " Ваш ИМТ " + str_IMT+ " Вес: " + str_Weight + " Рост: " +str_Length + " Возраст: " + str_Age);
+       // setIntent("test");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+  /*
+    private void setIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+      //  intent.setType("text/plain");
+          intent.putExtra("IMT", str_IMT);
+        //  intent.putExtra("Weight", str_Weight);
+         // intent.putExtra("Length", str_Length);
+      //   intent.putExtra("Age", str_Age);
+         text = str_IMT + str_Weight;
+        intent.putExtra(Intent.EXTRA_TEXT, str_Date + str_IMT);
+        shareActionProvider.setShareIntent(intent);
+    }
+*/
+   @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId()) {
+            case R.id.action_create_save:
+                //Код, выполняемый при выборе элемента Create Save
+                Intent intent = new Intent(this, SecondActivity.class);
+                intent.setType("text/plain");
+                intent.putExtra("IMT", str_IMT);
+                intent.putExtra("Weight", str_Weight);
+                intent.putExtra("Length", str_Length);
+                intent.putExtra("Age", str_Age);
+                intent.putExtra("Date", str_Date);
+             //   str_IMT_mail= str_Date + " Ваш ИМТ: " + str_IMT + " Вес: " + str_Weight + " Рост " + str_Length + " Возраст: " + str_Age;
+                startActivity(intent);
+            //    textViewW.setText(getString(R.string.name_text_table_detail_weight,str_weight));
+
+                return true;
+            case R.id.action_share:
+             /*   //Код, выполняемый при выборе элемента Share
+                Intent intent_mail = new Intent(Intent.ACTION_SENDTO);
+                intent_mail.setData(Uri.parse("mailto:")); // only email apps should handle this
+                //   intent_mail.setType("text/plain");
+                intent_mail.putExtra("IMT", str_IMT);
+                intent_mail.putExtra(Intent.EXTRA_TEXT,str_Date + " IMT  " + str_IMT + " Weight " + str_Weight);
+                startActivity(intent_mail);
+              */
+                Intent intent_mes = new Intent(Intent.ACTION_SEND);
+                intent_mes.setType("text/plain");
+                intent_mes.putExtra(Intent.EXTRA_TEXT, str_Date + " Ваш ИМТ: " + str_IMT + " Вес: " + str_Weight + " Рост " + str_Length + " Возраст: " + str_Age);
+//          page 143 always choose intent
+                String chooserTitle = "Sharing";
+                Intent chosenIntent = Intent.createChooser(intent_mes, chooserTitle);
+                startActivity(chosenIntent);
+                return true;
+
+            case R.id.action_history:
+                Intent intent_history = new Intent(this, History.class);
+                startActivity(intent_history);
+
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713 for advertisment 58fd171274ed00c90079860acbfcfda3
 //        test id ca-app-pub-3940256099942544/6300978111 for layout XML
 //        working real banner ca-app-pub-7279174300665421/8731793267
 
         MobileAds.initialize(this, "ca-app-pub-7279174300665421~3105181624");
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+       // mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
 
 
@@ -88,7 +212,8 @@ public class MainActivity extends AppCompatActivity {
 */
 
         textView = findViewById(R.id.result_out);
-        textViewAge = findViewById(R.id.text_table_age);
+        textViewTable = findViewById(R.id.text_table);
+       /* textViewAge = findViewById(R.id.text_table_age);
         textViewW = findViewById(R.id.text_table_weight);
         textViewL = findViewById(R.id.text_table_length);
         textViewTable3 = findViewById(R.id.text_table3);
@@ -97,10 +222,12 @@ public class MainActivity extends AppCompatActivity {
         textViewTable6 = findViewById(R.id.text_table6);
         textViewTable7 = findViewById(R.id.text_table7);
         textViewTable8 = findViewById(R.id.text_table8);
+        */
         final Button button = findViewById(R.id.calculate_btn);
         editTextW = findViewById(R.id.weight);
         editTextL = findViewById(R.id.length);
         editTextAge = findViewById(R.id.age);
+
 
 
 
@@ -188,20 +315,21 @@ public class MainActivity extends AppCompatActivity {
 
 
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
+
         public void onClick(View view) {
 
-//            MainActivity.this.getWindow().setSoftInputMode(android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
-            getWindow().setSoftInputMode(
-                    WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
-            );
+
+            KeyboardHide.hide(view); // Hide keyboard after click on button CALCULATE
             String weight = editTextW.getText().toString();
             String length = editTextL.getText().toString();
             String age = editTextAge.getText().toString();
+            str_Weight = weight;
+            str_Length = length;
+            str_Age = age;
 
-
-
-
+            String date = new SimpleDateFormat("dd-MM-yyyy HH:mm").format(Calendar.getInstance().getTime());
+            str_Date = date;
+           // str_Date = "MyDate";
 
             myweight = Double.parseDouble(weight);
             mylength = Double.parseDouble(length)/100;
@@ -210,82 +338,60 @@ public class MainActivity extends AppCompatActivity {
             if (mylength == 0)  {
                mylength = 1;
             }
-            myresult = myweight/(mylength * mylength); //calculatin IMT
+            myresult = myweight/(mylength * mylength); //calculating IMT
+            str_IMT =  String.format("%.2f",myresult); // making string format
 
-            textViewW.setText(getString(R.string.name_text_table_detail_weight,weight));
-            textViewL.setText(getString(R.string.name_text_table_detail_length,length));
+          //  textViewW.setText(getString(R.string.name_text_table_detail_weight,weight));
+         //   textViewL.setText(getString(R.string.name_text_table_detail_length,length));
             // highlighting age
-            textViewAge.setText(getString(R.string.name_text_table_detail_age,age));
-            textViewAge.setBackgroundColor(Color.GRAY);
+         //   textViewAge.setText(getString(R.string.name_text_table_detail_age,age));
+         //   textViewAge.setBackgroundColor(Color.GRAY);
 //            highlighting age tables
         //    if (myage >=19){
 //                textViewTable3.setBackgroundColor(Color.GRAY);
 //            }
             if (myage < 19) {
-                Toast.makeText(MainActivity.this, "Too young, to be in table of age",
+                Toast.makeText(MainActivity.this, R.string.too_young,
                       Toast.LENGTH_LONG).show();
-                textViewTable3.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable4.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable5.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable6.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable7.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable8.setBackgroundColor(Color.TRANSPARENT);
 
             }
             if ((myage >= 19) && (myage <= 24)){
+
+                textViewTable.setText( R.string.text_table3);
+
+                /*
                 textViewTable3.setBackgroundColor(Color.GRAY);
                 textViewTable4.setBackgroundColor(Color.TRANSPARENT);
                 textViewTable5.setBackgroundColor(Color.TRANSPARENT);
                 textViewTable6.setBackgroundColor(Color.TRANSPARENT);
                 textViewTable7.setBackgroundColor(Color.TRANSPARENT);
                 textViewTable8.setBackgroundColor(Color.TRANSPARENT);
-
+                */
             }
             if ((myage > 24) && (myage <= 34)){
-                textViewTable4.setBackgroundColor(Color.GRAY);
-                textViewTable3.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable5.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable6.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable7.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable8.setBackgroundColor(Color.TRANSPARENT);
-
+                textViewTable.setText(R.string.text_table4);
             }
             if ((myage > 34) && (myage <= 44)){
-                textViewTable5.setBackgroundColor(Color.GRAY);
-                textViewTable4.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable3.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable6.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable7.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable8.setBackgroundColor(Color.TRANSPARENT);
+                textViewTable.setText(R.string.text_table5);
+
             }
             if ((myage > 44) && (myage <= 54)){
-                textViewTable6.setBackgroundColor(Color.GRAY);
-                textViewTable4.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable5.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable3.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable7.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable8.setBackgroundColor(Color.TRANSPARENT);
+                textViewTable.setText(R.string.text_table6);
+
             }
             if ((myage > 54) && (myage <= 64)){
-                textViewTable7.setBackgroundColor(Color.GRAY);
-                textViewTable4.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable5.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable6.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable3.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable8.setBackgroundColor(Color.TRANSPARENT);
+                textViewTable.setText(R.string.text_table7);
+
             }
 
             if (myage > 64) {
-                textViewTable8.setBackgroundColor(Color.GRAY);
-                textViewTable4.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable5.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable6.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable7.setBackgroundColor(Color.TRANSPARENT);
-                textViewTable3.setBackgroundColor(Color.TRANSPARENT);
+                textViewTable.setText(R.string.text_table8);
+
             }
 
 
 //            String myresultStr = Double.toString(myresult);
+
             String myresultStrFormat = String.format("%.2f",myresult);
             textView.setText(getString(R.string.result_text,myresultStrFormat));
 
@@ -293,12 +399,15 @@ public class MainActivity extends AppCompatActivity {
                 textView.setTextColor(Color.RED);
                 }
             if ((myresult >= 19) && (myresult <=24)) {
-                textView.setTextColor(Color.WHITE);
+                textView.setTextColor(Color.BLACK);
             }
-            if ((myresult > 25) && (myresult <= 34)) {
-                textView.setTextColor(Color.YELLOW);
+            if ((myresult > 25) && (myresult <= 30)) {
+                textView.setTextColor(Color.BLUE);
             }
-                        if ((myresult > 34)) {
+            if ((myresult > 30) && (myresult <= 33)) {
+                textView.setTextColor(Color.MAGENTA);
+            }
+                        if ((myresult > 33)) {
                 textView.setTextColor(Color.RED);
             }
 
--- Tablo/app/src/main/java/com/arhsota/tablo/MainActivity.java
+++ Tablo/app/src/main/java/com/arhsota/tablo/MainActivity.java
@@ -1,8 +1,10 @@
-// не соответствует работа виртуального устройства и реального, таймер,при отсутствии фокуса и затем при появлении
-// его, на реальном устройстве сбрасывается
+// from stopwatch as pattern
+// 4 DMitry Siz
+// 2018, 2019
 
-package com.arhsota.stopwatch;
+package com.arhsota.tablo;
 
+import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Handler;
@@ -13,46 +15,73 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import static com.arhsota.stopwatch.R.id.radioButton5;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
-public class StopwatchActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     //Количество секунд на секундомере
     private int seconds = 300;
     private int secondsChoice = 300;
+    //Score
     private int scorePlayer1 = 0;
     private int scorePlayer2 = 0;
     //Секундомер работает?
     private boolean running;
     private boolean wasRunning;
-    private boolean player1;
-    private boolean player2;
+
     private RadioButton radioButton5;
     private RadioButton radioButton10;
 
-    Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+    private AdView mAdView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stopwatch);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this,
+                "ca-app-pub-7279174300665421~2703649226");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
        // seconds = secondsChoice;
         if (savedInstanceState != null) {
             seconds = savedInstanceState.getInt("seconds");
+            secondsChoice = savedInstanceState.getInt("secondsChoice");
             running = savedInstanceState.getBoolean("running");
             wasRunning = savedInstanceState.getBoolean("wasRunning");
-            player1 = savedInstanceState.getBoolean("player1");
+
+            scorePlayer1 = savedInstanceState.getInt("scorePlayer1");
+            scorePlayer2 = savedInstanceState.getInt("scorePlayer2");
         }
         runTimer();
+        // For changing orientation
+        TextView scoreViewPlayer1 = (TextView) findViewById(R.id.leftScore);
+        String strScorePlayer1 = String.format("%d",scorePlayer1);
+        scoreViewPlayer1.setText(strScorePlayer1);
+
+        TextView scoreViewPlayer2 = (TextView) findViewById(R.id.rightScore);
+        String strScorePlayer2 = String.format("%d",scorePlayer2);
+        scoreViewPlayer2.setText(strScorePlayer2);
 
     }
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState); // этой строки нет в книге
-     //   seconds = secondsChoice;
+
         savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putInt("secondsChoice",secondsChoice);
         savedInstanceState.putBoolean("running", running);
         savedInstanceState.putBoolean("wasRunning", wasRunning);
-        savedInstanceState.putBoolean("player1", player1);
+      //  savedInstanceState.putBoolean("player1", player1);
+        savedInstanceState.putInt("scorePlayer1", scorePlayer1);
+        savedInstanceState.putInt("scorePlayer2", scorePlayer2);
+        seconds = secondsChoice;
     }
 //    page 176
     @Override
@@ -85,7 +114,7 @@ public class StopwatchActivity extends AppCompatActivity {
     private void runTimer() {
         final TextView timeView = (TextView) findViewById(R.id.time_view);
         final Handler handler = new Handler();
-        seconds = secondsChoice;
+      //  seconds = secondsChoice;
 
         handler.post(new Runnable() {
             @Override
@@ -96,8 +125,11 @@ public class StopwatchActivity extends AppCompatActivity {
                 int secs = seconds % 60;
                 String time = String.format("%d:%02d:%02d",hours, minutes, secs);
                 if (seconds == 0) {
-                    Toast.makeText(StopwatchActivity.this, "Game OVER",
+                    Toast.makeText(MainActivity.this, "Game OVER",
                             Toast.LENGTH_SHORT).show();
+                    Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
+                    mp.start();
                     }
                 timeView.setText(time);
                 if ((running) && (seconds > 0)) {
@@ -151,4 +183,36 @@ public class StopwatchActivity extends AppCompatActivity {
     }
 
 
+    public void onClickScoreAdj1(View view) {
+        if (scorePlayer1 > 0) {
+            TextView scoreViewPlayer1 = (TextView) findViewById(R.id.leftScore);
+            scorePlayer1 -= 1;
+            String strScorePlayer1 = String.format("%d", scorePlayer1);
+            scoreViewPlayer1.setText(strScorePlayer1);
+        }
+
+    }
+
+    public void onClickScoreAdj2(View view) {
+        if (scorePlayer2 > 0) {
+            TextView scoreViewPlayer2 = (TextView) findViewById(R.id.rightScore);
+            scorePlayer2 -= 1;
+            String strScorePlayer2 = String.format("%d", scorePlayer2);
+            scoreViewPlayer2.setText(strScorePlayer2);
+        }
+    }
+
+    public void onClickResetScore(View view) {
+
+    //  Reset Score to zero
+        scorePlayer1 = 0;
+        scorePlayer2 = 0;
+        TextView scoreViewPlayer1 = (TextView) findViewById(R.id.leftScore);
+        String strScorePlayer1 = String.format("%d",scorePlayer1);
+        scoreViewPlayer1.setText(strScorePlayer1);
+
+        TextView scoreViewPlayer2 = (TextView) findViewById(R.id.rightScore);
+        String strScorePlayer2 = String.format("%d",scorePlayer2);
+        scoreViewPlayer2.setText(strScorePlayer2);
+    }
 }
